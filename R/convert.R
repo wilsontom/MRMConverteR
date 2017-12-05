b881374ace29cb57190480919aab50fdb85007cd
wilsontom <wilsontom89@gmail.com>
2017-12-05 13:40:22 +0000

    overhaul; use dplyr and account for polarity

--- R/convert.R
+++ R/convert.R
@@ -9,135 +9,140 @@
 #'
 #' @export
 #' @importFrom stats aggregate
+#' @importFrom dplyr %>% select bind_rows group_by summarise ungroup arrange rename
 #' @author Tom Wilson \email{tpw2@@aber.ac.uk}
 
+convert <- function(input, outpath){
 
-convert <- function(input, outpath)
-{
-  if (tools::file_ext(input) != 'mzML') {
-    stop(deparse(substitute(input)), ' is not a .mzML file', call. = FALSE)
-  }
+if (tools::file_ext(input) != 'mzML') {
+  stop(deparse(substitute(input)), ' is not a .mzML file', call. = FALSE)
+}
 
-  open_raw <- mzR::openMSfile(input, backend = 'pwiz')
+open_raw <- mzR::openMSfile(input, backend = 'pwiz')
 
-  if (length(open_raw) != 0) {
-    warning('length is > 0')
-  }
+if (length(open_raw) != 0) {
+  warning('length is > 0')
+}
 
-  chrom_raw <- mzR::chromatograms(open_raw)
+chrom_raw <- mzR::chromatograms(open_raw)
 
-  ind_name <- lapply(chrom_raw, function(x)
-    (names(x)))
+filter_id <- purrr::map(chrom_raw, ~ {
+  names(.)[[2]]
+}) %>% unlist(.)
 
-  if (ind_name[[1]][2] == 'TIC') {
-    chrom_raw[[1]] <- NULL
-  }
+filter_polarity <- purrr::map_dbl(filter_id, extract_polarity)
 
+filter_prec <- purrr::map_dbl(filter_id, extract_precursor)
 
-  for (i in seq_along(chrom_raw)) {
-    chrom_raw[[i]] <-
-      data.frame(chrom_raw[[i]], mz = rep(names(chrom_raw[[i]])[2]))
-  }
+filter_prod <- purrr::map_dbl(filter_id, extract_prodcut)
 
-
-  mz_raw <- lapply(chrom_raw, function(x)
-    (clean_index(x$mz)))
-
-  mz_split <- lapply(mz_raw, split_index)
-
-  chrom_clean <- NULL
-  for (i in seq_along(chrom_raw)) {
-    chrom_clean[[i]] <-
-      data.frame(
-        rt = chrom_raw[[i]]$time,
-        mz = mz_split[[i]]$ms1,
-        int = chrom_raw[[i]][, 2],
-        mzagg = paste0(mz_split[[i]]$ms1, '-', mz_split[[i]]$ms2)
-      )
-  }
-
-  # assume a cycle time of 1.0 Seconds
-
-  for (i in seq_along(chrom_clean)) {
-    chrom_clean[[i]]$rt <- round(chrom_clean[[i]]$rt, digits = 1)
-  }
-
-  # scan_time <- as.numeric(names(chrom_split))
-
-  chrom_all <- do.call('rbind', chrom_clean)
-
-  chrom_split <- split(chrom_all, chrom_all$rt)
-
-  scan_time <- as.numeric(names(chrom_split))
-
-  # sum int across agg peak
-  chrom_agg <- NULL
-  for (i in seq_along(chrom_split)) {
-    chrom_agg[[i]] <-
-      aggregate(chrom_split[[i]]$int, by = list(chrom_split[[i]]$mzagg), sum)
-    names(chrom_agg[[i]]) <- c('mz', 'int')
-  }
-
-
-  for (i in seq_along(chrom_agg)) {
-    chrom_agg[[i]]$mz <- strip_ms2(chrom_agg[[i]]$mz)
-  }
-
-
-
-  chrom_agg <- lapply(chrom_agg, function(x)
-    (x[order(x$mz), ]))
-
-
-  hdnm <- header_names()
-
-  hd_tmp <-
-    data.frame(matrix(nrow = length(chrom_agg), ncol = length(hdnm)))
-  names(hd_tmp) <- hdnm
-
-  hd_tmp[, 'seqNum'] <-
-    hd_tmp[, 'acquisitionNum'] <-
-    seq(from = 1,
-        to = nrow(hd_tmp),
-        by = 1)
-  hd_tmp[, 'msLevel'] <- rep(1)
-  hd_tmp[, 'polarity'] <- rep(1)
-
-  peak_count <- vapply(chrom_agg, nrow, length(chrom_agg))
-
-  hd_tmp[, 'peaksCount'] <- peak_count
-
-  tic <- unlist(lapply(chrom_agg, function(x)
-    (sum(x$int))))
-  bpi <- unlist(lapply(chrom_agg, function(x)
-    (max(x$int))))
-  bpmz <-
-    unlist(lapply(chrom_agg, function(x)
-      (x$mz[which(x$int == max(x$int))])))
-
-  hd_tmp[, 'totIonCurrent'] <- as.numeric(tic)
-  hd_tmp[, 'basePeakIntensity'] <- as.numeric(bpi)
-  hd_tmp[, 'basePeakMZ'] <- as.numeric(bpmz)
-
-  hd_tmp[, 'retentionTime'] <- scan_time
-
-  hd_tmp[is.na(hd_tmp)] <- 0
-
-  if ('filterString' %in% names(hd_tmp)) {
-    hd_tmp[, 'filterString'] <- as.character(hd_tmp[, 'filterString'])
-  }
-  destfile <- paste0(outpath, '/', 'convert_', basename(input))
-  destfile <- gsub('.mzML', '.mzML', destfile)
-
-  for (i in seq_along(chrom_agg)) {
-    chrom_agg[[i]] <- as.matrix(chrom_agg[[i]])
-  }
-
-  mzR::writeMSData(
-    data = chrom_agg,
-    filename = destfile,
-    header = hd_tmp,
-    outformat = 'mzml'
+filter_index <-
+  tibble(
+    polarity = filter_polarity,
+    precursor = filter_prec,
+    product = filter_prod,
+    chromidx = seq(from = 1, to = length(chrom_raw))
   )
 
+idn_out <-
+  which(filter_index[, 'polarity'] == 0 &
+          filter_index[, 'precursor'] == 0 & filter_index[, 'product'] == 0)
+
+if(length(idn_out > 0)) {
+  filter_index <- filter_index[-idn_out]
+  chrom_raw[[idn_out]] <- NULL
+}
+
+chrom_tmp <- NULL
+for (i in seq_along(chrom_raw)) {
+  chrom_tmp[[i]] <-
+    data.frame(
+      rt = chrom_raw[[i]]$time,
+      int = chrom_raw[[i]][, 2],
+      polarity = rep(filter_index[i, 'polarity']),
+      precursor = rep(filter_index[i, 'precursor']),
+      product = rep(filter_index[i, 'product'])
+    )
+}
+
+chrom_all <- chrom_tmp %>% bind_rows()
+chrom_all[, 'rt'] <- round(chrom_all[, 'rt'] * 60, digits = 0)
+
+chrom_agg <-
+  chrom_all %>% group_by(., polarity, rt, product) %>% summarise(., int = sum(int)) %>% ungroup(.) %>%
+  mutate(., spf = paste0(.$rt, ':', .$polarity)) %>% arrange(., rt)
+
+chrom_split <- split(chrom_agg, chrom_agg$spf)
+
+chrom_pol <-
+  purrr::map(chrom_split, ~ {
+    unique(.$polarity)
+  }) %>% unlist(.)
+chrom_rt <-  purrr::map(chrom_split, ~ {
+  unique(.$rt)
+}) %>% unlist(.)
+peaks_count <- purrr::map(chrom_split, ~ {
+  nrow(.)
+}) %>% unlist(.)
+
+bpi <- purrr:::map(chrom_split, ~ {
+  max(.$int)
+}) %>% unlist(.)
+tic <- purrr:::map(chrom_split, ~ {
+  sum(.$int)
+}) %>% unlist(.)
+bpmz <-
+  purrr:::map(chrom_split, ~ {
+    .$product[which(.$int == max(.$int))]
+  }) %>% unlist(.)
+
+
+# this is too slow
+peaks <-
+  purrr:::map(chrom_split, ~ {
+    select(., -c(polarity, rt, spf)) %>% rename(., mz = product) %>% as.matrix(.)
+  })
+
+
+# create the `header``
+
+names(peaks) <- NULL
+
+
+hdnm <- header_names()
+
+hd_tmp <-
+  data.frame(matrix(nrow = length(peaks), ncol = length(hdnm)))
+names(hd_tmp) <- hdnm
+
+hd_tmp[, 'seqNum'] <-
+  hd_tmp[, 'acquisitionNum'] <-
+  seq(from = 1,
+      to = nrow(hd_tmp),
+      by = 1)
+
+hd_tmp[, 'polarity'] <- chrom_pol
+hd_tmp[, 'retentionTime'] <- chrom_rt
+hd_tmp[, 'peaksCount'] <- peaks_count
+hd_tmp[, 'totIonCurrent'] <- as.numeric(tic)
+hd_tmp[, 'basePeakIntensity'] <- as.numeric(bpi)
+hd_tmp[, 'basePeakMZ'] <- as.numeric(bpmz)
+
+hd_tmp[is.na(hd_tmp)] <- 0
+
+if ('filterString' %in% names(hd_tmp)) {
+  hd_tmp[, 'filterString'] <- as.character(hd_tmp[, 'filterString'])
+}
+
+destfile <- paste0(outpath, '/', 'convert_', basename(input))
+destfile <- gsub('.mzML', '.mzML', destfile)
+
+mzR::writeMSData(
+  data = peaks,
+  filename = destfile,
+  header = hd_tmp,
+  outformat = 'mzml'
+)
+
+return(invisible(NULL))
 }
