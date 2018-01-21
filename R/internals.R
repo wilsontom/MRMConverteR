6e1248af265956f8781e2e7209e217e91a030d68
wilsontom <wilsontom89@gmail.com>
2018-01-21 17:31:07 +0000

    remove old internals

--- R/internals.R
+++ R/internals.R
@@ -2,50 +2,6 @@ globalVariables(c('.', 'rt', 'int', 'product', 'polarity', 'mz'))
 
 
 #' @keywords internal
-#'
-
-extract_polarity <- function(x)
-{
-  x <- as.character(x)
-
-  if(length(grep('X..', x)) == 1){
-    p <- -1
-  }else{
-    p <- 1
-  }
-  if(x == 'TIC'){
-    p <- 0
-  }
-  return(p)
-}
-
-#' @keywords internal
-extract_precursor <- function(x)
-{
-  if (x == 'TIC') {
-    return(0)
-  } else{
-    x <- gsub('X..SRM.SIC.|SRM.SIC.', '', x)
-    xa <- strsplit(x, '\\.')
-    return(as.numeric(paste0(xa[[1]][1], '.', xa[[1]][2])))
-  }
-}
-
-#' @keywords internal
-extract_prodcut <- function(x)
-{
-  if (x == 'TIC') {
-    return(0)
-  } else{
-    x <- gsub('X..SRM.SIC.|SRM.SIC.', '', x)
-    xa <- strsplit(x, '\\.')
-    return(as.numeric(paste0(xa[[1]][3], '.', xa[[1]][4])))
-  }
-}
-
-
-
-#' @keywords internal
 header_names <- function(x){
 
 	c("seqNum",                 "acquisitionNum",
