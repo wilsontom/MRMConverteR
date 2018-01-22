4b625fc82896288a4b0ff62e12bcff7e79e65fe7
wilsontom <wilsontom89@gmail.com>
2018-01-22 14:24:07 +0000

    update docs

--- R/save_mzml.R
+++ R/save_mzml.R
@@ -1,19 +1,19 @@
 #' Save Converted mzML
 #'
-#' @param peaks a list of \code{peaks} and \code{header} which has been created using the \code{convert_mzml} function
+#' @param input a list of \code{peaks} and \code{header} which has been created using the \code{convert_mzml} function
 #' @param filename a character string of the filename to save converted files to.
 #'
 #' @export
 #' @author Tom Wilson \email{tpw2@@aber.ac.uk}
 
-save_mzml <- function(peaks,filename)
+save_mzml <- function(input,filename)
 {
     filename <- paste0(filename, '.mzML')
 
     mzR::writeMSData(
-    object = peaks$peaks,
+    object = input$peaks,
     file = filename,
-    header = peaks$header,
+    header = input$header,
     outformat = 'mzml'
   )
 
